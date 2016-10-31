76f08f965a4bf6e01f041fec74a45d093c15c1e9
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-10-31 14:27:57 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="podio-mirror",
+    version="0.1",
+    description="Wrapper for part of the Podio API with interfaces for caching data locally",
+    author="Patrik Sletmo",
+    author_email="patrik.sletmo@gmail.com",
+    url="https://github.com/patriksletmo/podio-mirror",
+    license="MIT",
+    packages=["podio-mirror"],
+    install_requires=["requests"]
+)
